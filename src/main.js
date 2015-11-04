17c5fbd3dff8ef85f14a6874d4ef3943b99095c0
Artem Kadeev <a.kadeev@gmail.com>
2015-11-04 07:23:02 +0300

    Basic implementation of static methods invocation added

--- src/main.js
+++ src/main.js
@@ -1,28 +1,112 @@
-function createEntity(name, parent) {
+function loadJvm() {
+  return new Promise(
+    (resolve, reject) => {
+      // create a virtual file system for frontend JS
+      var mfs = new BrowserFS.FileSystem.MountableFileSystem();
+      var fs = BrowserFS.BFSRequire('fs');
+      BrowserFS.initialize(mfs);
 
+      // store files of folder /tmp in memory
+      mfs.mount('/tmp', new BrowserFS.FileSystem.InMemory());
+      // store files of folder /home in LocalStorage
+      mfs.mount('/home', new BrowserFS.FileSystem.LocalStorage());
+      // retrieve files of folder /sys via XmlHttpRequest according records of doppio/listings.json
+      // all this file-paths will be prefixed with 'doppio/'
+      mfs.mount('/sys', new BrowserFS.FileSystem.XmlHttpRequest('listings.json', 'doppio/'));
+
+      // initialise JVM with next settings
+      new doppio.JVM({
+        bootstrapClasspath: ['/sys/vendor/java_home/classes'],
+        classpath: ['/sys'],
+        javaHomePath: '/sys/vendor/java_home',
+        extractionPath: '/tmp',
+        nativeClasspath: ['/sys/src/natives'],
+        assertionsEnabled: false
+      }, (err, jvmObject) => {
+        resolve(jvmObject);
+      });
+    }
+  );
+}
+
+function getClass(classObject) {
+  const className = classObject._identifier + ';';
+  return new Promise(
+    (resolve, reject) => {
+      if (classObject.cache !== undefined) {
+        resolve(classObject.cache);
+      } else {
+        console.log('LOG: Start loading class: ' + className);
+        jvm.getSystemClassLoader().initializeClass(jvm.firstThread, className, cls => {
+          classObject.cache = cls;
+          resolve(cls);
+        });
+      }
+    }
+  );
+}
+
+function runMethod(methodObject, argumentsList) {
+  return new Promise(
+    (resolve, reject) => {
+      getClass(methodObject._parent).then(cls => {
+        for (var i = 0; i < cls.methods.length; i++) {
+          var method = cls.methods[i];
+          if (method.name === methodObject._name && method.num_args === argumentsList.length) {
+            // TODO parse different type of arguments and return type
+            console.log('LOG: Start running method: ' + method.name);
+            jvm.firstThread.runMethod(method, argumentsList, (e, rv) => {
+              var message = rv.fields['Ljava/lang/String;value'].array.map(
+                c => { return String.fromCharCode(c); }
+              ).join('');
+              resolve(message);
+            });
+            break;
+          }
+        }
+      });
+    }
+  );
+}
+
+function createEntity(name, parent) {
   // We don't now in advance is it a function or just an Object
   // But objects cannot be called, so it is a function
   const object = function() {};
   object._parent = parent;
-  object._identifier = (parent === null ? "" : parent._identifier + ".") + name,
+  object._name = name;
+  object._identifier = (parent === null ? "" : parent._identifier + "/") + name;
   object._call = function(thisArg, argumentsList) {
-    console.log("Function called: " + this._identifier + "(" + argumentsList + ")");
-  }
+    return new Promise(
+      (resolve, reject) => {
+        runMethod(object, argumentsList).then(rv => resolve(rv));
+      }
+    );
+  };
 
   const proxy = new Proxy(object, {
-    get: function(target, property) {
+    get: (target, property) => {
       if (!target.hasOwnProperty(property)) {
         target[property] = createEntity(property, target);
-        console.log("New entity created: " + target[property]._identifier);
       }
       return target[property];
     },
-    apply: function(target, thisArg, argumentsList) {
-      target._call(thisArg, argumentsList);
+    apply: (target, thisArg, argumentsList) => {
+      return target._call(thisArg, argumentsList);
     }
   });
 
   return proxy;
 }
 
-const java = createEntity("java", null);
+var jvm;
+
+var java;
+
+function executeWithJvm(userCode) {
+  loadJvm().then(jvmObject => {
+    jvm = jvmObject;
+    java = createEntity("Ljava", null);
+    userCode();
+  });
+}
