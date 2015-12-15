931bf35e1f3f0572ea1e2db0b3d97c93beecd86b
hrjet9 <hrjet9@gmail.com>
2015-12-15 11:30:58 +0530

    Added file read test

--- test/classes/Main.java
+++ test/classes/Main.java
@@ -6,4 +6,25 @@ public class Main {
   public static void main(String args[]) {
     System.out.println("hello world2");
   }
+
+  public static String readFile(String fileName) {
+
+    try(final java.io.FileReader fr = new java.io.FileReader(fileName)) {
+      boolean done = false;
+      final StringBuffer result = new StringBuffer();
+      while (!done) {
+        int c = fr.read();
+        done = c == -1;
+        if (!done) {
+          result.append((char) c);
+        }
+      }
+
+      return result.toString();
+    } catch (java.io.IOException e) {
+      System.out.println("Exception while reading from file");
+      return null;
+    }
+  }
+
 }
