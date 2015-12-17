330d1f1b3ae0ab031ab7e855b42f90feaec0b165
Sergey Reymerov <sergeyreym@gmail.com>
2015-12-18 01:50:52 +0800

    Initial java compile script commit

--- src/JavaSourceFile.js
+++ src/JavaSourceFile.js
@@ -3,8 +3,8 @@ import JavaFile from './JavaFile';
 //const path = global.BrowserFS.BFSRequire('path');
 
 class JavaSourceFile extends JavaFile  {
-  constructor(javaPolyLoader, script) {
-    super(javaPolyLoader, script);
+  constructor(javaPoly, script) {
+    super(javaPoly, script);
 
     /**
      * Source code of Java file
@@ -12,13 +12,23 @@ class JavaSourceFile extends JavaFile  {
      */
     this.source = script.text;
 
-    this.classname = 'TestCompile';
+    let classInfo = JavaSourceFile.detectClassAndPackageNames(this.source);
+
+    this.classname = classInfo.class;
+
+    this.packagename = classInfo.package;
 
     let path = global.BrowserFS.BFSRequire('path');
 
-    this.filename = path.join(javaPolyLoader.options.storageDir, this.classname + '.java');
+    this.filename = path.join(
+      javaPoly.options.storageDir, 
+      this.packagename ? this.packagename.replace(/\./g, '/') : '.',
+      this.classname + '.java'
+    );
 
-    this.javaPolyLoader.fs.writeFile(this.filename, this.source, err => {
+    this.javaPoly.fsext.rmkdirSync(path.dirname(this.filename));
+
+    this.javaPoly.fs.writeFile(this.filename, this.source, err => {
       if (err) {
         console.error(err);
       }
@@ -26,14 +36,33 @@ class JavaSourceFile extends JavaFile  {
   }
 
   /**
+   * This functions parse Java source file and detects its name and package
+   * @param  {String} source Java source
+   * @return {Object}        Object with fields: package and class
+   */
+  static detectClassAndPackageNames(source) {
+    // this regexp removes all comments in source
+    source = source.replace(/(\/\*[^]*\*\/|\/\/[^\n]*\n)/gi, ' ');
+
+    let className = source.match(/class\s+([^\s\{]+)(\s|\{)/);
+    let packageName = source.match(/package\s+([^\s;]+)\s*;/)
+
+    return {
+      package: packageName ? packageName[1] : null,
+      class:   className   ? className[1]   : null
+    }
+  }
+
+  /**
    * Compile file from source property. It depends on global Java object.
    * @return {Promise} to detect when it finishes
    */
   compile() {
+    console.log('Source file for:', this.classname, this.packagename);
     return new Promise((resolve, reject) => {
       global.window.Java.type('javax.tools.ToolProvider').then( ToolProvider => {
         ToolProvider.getSystemJavaCompiler().then(JavaCompiler => {
-          // console.log(this.javaPolyLoader.jvm);
+          // console.log(this.javaPoly.jvm);
           // console.log(JavaCompiler);
           // JavaCompiler.run(null, null, null, this.filename).then(result => {
             // if (result === 0) {
