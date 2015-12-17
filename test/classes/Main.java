c882261d93bba36ce5e36e007d1931d2660ef8ad
hrjet9 <hrjet9@gmail.com>
2015-12-17 17:36:08 +0530

    Only public static methods should be accessible in class wrapper
    Closes #36

--- test/classes/Main.java
+++ test/classes/Main.java
@@ -32,4 +32,30 @@ public class Main {
   public static boolean checkLength(final String str, final int n) {
     return str.length() == n;
   }
+
+  /* private method defined only to check accessibilty */
+  private static void privateMethod() {
+    throw new RuntimeException("Should never be called");
+  }
+
+  /* protected method defined only to check accessibilty */
+  protected static void protectedMethod() {
+    throw new RuntimeException("Should never be called");
+  }
+
+  /* private instance method defined only to check accessibilty */
+  private void privateInstanceMethod() {
+    throw new RuntimeException("Should never be called");
+  }
+
+  /* protected instance method defined only to check accessibilty */
+  protected void protectedInstanceMethod() {
+    throw new RuntimeException("Should never be called");
+  }
+
+  /* public instance method defined only to check accessibilty */
+  public void publicInstanceMethod() {
+    throw new RuntimeException("Should never be called");
+  }
+
 }
