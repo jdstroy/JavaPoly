a44dc5cc08722179b6fb838b66a6edeb4abaaff9
hrjet9 <hrjet9@gmail.com>
2015-12-22 10:44:45 +0530

    easy way to select worker type during testing

--- src/main.test.js
+++ src/main.test.js
@@ -1,4 +1,23 @@
 import JavaPoly from './core/JavaPoly';
 
+/* Parses URL search string. Eg of valid input: "?worker=true&dispatch=fast" */
+function getParams(searchString) {
+  const result = {};
+  if (searchString.startsWith("?")) {
+    const fields = searchString.substring(1).split("&");
+    for (const f of fields) {
+      const pair = f.split("=");
+      if (pair.length == 2) {
+        result[pair[0]] = pair[1];
+      }
+    }
+  }
+  return result;
+}
+
+const params = getParams(window.location.search);
+
+const config = params.worker ? {worker:'build/javapoly_worker.js'} : {};
+
 // Create main object that will be accessed via global objects J and Java
-global.window.javapoly = new JavaPoly();
+global.window.javapoly = new JavaPoly(config);
