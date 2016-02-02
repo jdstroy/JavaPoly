460ac01ac319a775324b3bd9b968d7ea6ac533d7
coderrooftrellen <coderrooftrellen@gmail.com>
2016-02-02 22:47:49 +0800

    add test case for proxy access of dynamically added classes.

--- test/units/dynamicAdd.js
+++ test/units/dynamicAdd.js
@@ -1,4 +1,5 @@
 function testDynamicAdd() {
+  var isProxySupported = (typeof Proxy !== 'undefined');
   describe('dynamically addClass', function(){
     it('add jar', function(){
       return addClass('/jars/commons-codec-1.10.jar').then(function(addClassResult){
@@ -45,6 +46,49 @@ function testDynamicAdd() {
         });
       });
     });
+
+    if (isProxySupported) {
+      describe('Proxy access of dynamically added Class', function(){
+
+        it('proxy access of added jar, ', function(){
+          return addClass('/jars/commons-codec-1.10.jar').then(function(addClassResult){
+            return J.org.apache.commons.codec.binary.StringUtils.equals('a','a').then(function (result){
+              expect(result).toEqual(true);
+              });
+          });
+        });
+
+        it('proxy access of added class', function(){
+          return addClass('/classes/Main2.class').then(function(addClassResult){
+            return J.Main2.test().then(function(result) {
+              expect(result).toEqual('test message in Main2');
+              });
+            });
+        });
+
+        it('proxy access of added source code', function() {
+          return addClass('/classes/Main3.java').then(function(addClassResult){
+            return J.com.javapoly.test.Main3.testIt().then(function(result) {
+              expect(result).toEqual('Main3::testIt()');
+            });
+          });
+        });
+
+        it('add embedded source code', function() {
+          var mainJavaSource = `package com.javapoly.test;
+            public class Main4 {
+            public static String testIt() {
+              return "Main4:testIt()";
+            }
+          }`;
+          return addClass(mainJavaSource).then(function(addClassResult){
+            return J.com.javapoly.test.Main4.testIt().then(function(result) {
+              expect(result).toEqual('Main4:testIt()');
+            });
+          });
+        });
+      });
+    }
 
   });
 }
