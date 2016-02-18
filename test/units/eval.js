e547b50194fda5697aae4a67b5fa0cbe7faaadd8
hrjet9 <hrjet9@gmail.com>
2016-02-18 23:41:01 +0530

    Moved reflection tests to separate file

--- test/units/eval.js
+++ test/units/eval.js
@@ -9,31 +9,6 @@ function testEval() {
       });
     });
 
-    if (!isWorkerBased) {
-      it('should reflect js object into java', function() {
-        return Java.type("EvalTest").then(function(EvalTest) {
-          var stringTest = EvalTest.getProperty({name1: "xyz", name2: 10}, "name1").then(function(name1Val) {
-            expect(name1Val).toBe("xyz");
-          });
-          var numberTest = EvalTest.getProperty({name1: "xyz", name2: 10}, "name2").then(function(name2Val) {
-            expect(name2Val).toBe(10);
-          });
-          var deepObjTest = EvalTest.getProperty({name1: "xyz", name3: {inner:"pqr"}}, "name3").then(function(name3Val) {
-            expect(name3Val.inner).toBe("pqr");
-          });
-          return Promise.all([stringTest, numberTest, deepObjTest]);
-        });
-      });
-
-      it('should reflect js object from java', function() {
-        return Java.type("com.javapoly.Eval").then(function(Eval) {
-          return Eval.eval('({name1: "xyz", name2: 10, name3: {inner: "music"}})').then(function(result) {
-            expect(result.name1).toBe("xyz");
-            expect(result.name3.inner).toBe("music");
-          });
-        });
-      });
-    }
   });
 }
 
