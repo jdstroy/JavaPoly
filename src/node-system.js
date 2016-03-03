f6e8c4a49e460ea2b6b2928eae1f46913c14be52
Russkih Anatoly <titanofdarkness@gmail.com>
2016-03-03 21:50:08 +1000

    Add comment

--- src/node-system.js
+++ src/node-system.js
@@ -3,4 +3,5 @@ import JavaPoly from './core/JavaPolyNodeSystem';
 // Create main object that will be accessed via global objects J and Java
 global.JavaPoly = JavaPoly;
 
+// For creating this code as Node module
 module.exports = JavaPoly;
