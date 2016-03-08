44a083f45abda52af9489f6632e899b239fb93b7
Russkih Anatoly <titanofdarkness@gmail.com>
2016-03-08 14:12:42 +1000

    Fix comments

--- src/node-doppio.js
+++ src/node-doppio.js
@@ -3,5 +3,5 @@ import JavaPoly from './core/JavaPolyNodeDoppio';
 // Create main object that will be accessed via global objects J and Java
 global.JavaPoly = JavaPoly;
 
-// For creating this code as Node module
+// For running this code as Node module
 module.exports = JavaPoly;
