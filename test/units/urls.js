ac7cc9b8650b9770b3c725576b07a9d36f5f8eec
hrjet9 <hrjet9@gmail.com>
2016-03-08 15:56:23 +0530

    minor: refactor url test to have a before() clause

--- test/units/urls.js
+++ test/units/urls.js
@@ -16,25 +16,27 @@ function readFromIS(is, callback) {
 function testUrls() {
   if (!isWorkerBased) {
     describe('URLs', function() {
-      it('should fetch data', function() {
+      before(() => {
         return Java.type("com.javapoly.XHRUrlStreamHandlerFactory").then(function(XHRUrlStreamHandlerFactory) {
-          return XHRUrlStreamHandlerFactory.register().then(function() {
-            return Java.type("java.net.URL").then(function(URL) {
-              return new URL(window.location.origin + "/simpleResponse.bin").then(function(url) {
-                return url.openConnection().then(function(urlConnection) {
-                  var headerPromise = urlConnection.getHeaderField('content-type').then(function(contentType) {
-                    expect(contentType).toBe('application/octet-stream');
-                  });
-                  var contentPromise = urlConnection.getInputStream().then(function(is) {
-                    return readFromIS(is).then(function(content) {
-                      expect(content[0]).toBe(97);
-                      expect(content[1]).toBe(98);
-                      expect(content[2]).toBe(99);
-                    });
-                  });
-                  return Promise.all([headerPromise, contentPromise]);
+          return XHRUrlStreamHandlerFactory.register();
+        });
+      });
+
+      it('should fetch data', function() {
+        return Java.type("java.net.URL").then(function(URL) {
+          return new URL(window.location.origin + "/simpleResponse.bin").then(function(url) {
+            return url.openConnection().then(function(urlConnection) {
+              var headerPromise = urlConnection.getHeaderField('content-type').then(function(contentType) {
+                expect(contentType).toBe('application/octet-stream');
+              });
+              var contentPromise = urlConnection.getInputStream().then(function(is) {
+                return readFromIS(is).then(function(content) {
+                  expect(content[0]).toBe(97);
+                  expect(content[1]).toBe(98);
+                  expect(content[2]).toBe(99);
                 });
               });
+              return Promise.all([headerPromise, contentPromise]);
             });
           });
         });
