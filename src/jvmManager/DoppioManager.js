7cae9ffb82eebfd2b79129fce7ca7c1e70b003d9
Sergey Reymerov <sergeyreym@gmail.com>
2016-03-29 01:08:28 +0800

    #46 Small code enhancements into DoppioManager.js

--- src/jvmManager/DoppioManager.js
+++ src/jvmManager/DoppioManager.js
@@ -75,89 +75,41 @@ class DoppioManager {
     });
 
   }
-
-  mountJava(src) {
+  
+  _mountJava(src, fRegJarPath) {
     const Buffer = global.BrowserFS.BFSRequire('buffer').Buffer;
     const options = this.getOptions();
-    this.bfsReady.then(() => {
-      this.mountHub.push(
-        new Promise((resolve, reject) => {
-          if (CommonUtils.isZipFile(src)) {
-            return this.mountFileViaXHR(src).then(
-              (jarStorePath) => {this.classpath.push(jarStorePath); resolve();},
-              reject );
-          } else {
-            // remote file, we need to download the file data of that url and parse the type.
-            CommonUtils.xhrRetrieve(src, "arraybuffer").then(fileData => {
-              const fileDataBuf = new Buffer(fileData);
 
-              // remote java class/jar file
-              if (CommonUtils.isClassFile(fileDataBuf)){
-                //return WrapperUtil.dispatchOnJVM(this, 'FS_MOUNT_CLASS', 10, {src:script.src});
-                return this.writeRemoteClassFileIntoFS(src, fileDataBuf).then(resolve, reject);
-              } else if (CommonUtils.isZipFile(fileDataBuf)) {
-                //return WrapperUtil.dispatchOnJVM(this, 'FS_MOUNT_JAR', 10, {src:script.src});
-                return this.writeRemoteJarFileIntoFS(src, fileDataBuf).then(
-                  (jarStorePath) => { this.classpath.push(jarStorePath); resolve(); },
-                  reject );
-              }
-
-              // remote java source code file
-              const classInfo = CommonUtils.detectClassAndPackageNames(fileDataBuf.toString());
-              if (classInfo && classInfo.class ){
-                const className = classInfo.class;
-                const packageName = classInfo.package;
-                return WrapperUtil.dispatchOnJVM(
-                    this.javapoly, "FILE_COMPILE", 10,
-                    [className, packageName ? packageName : "", options.storageDir, fileDataBuf.toString()], resolve, reject
-                  );
-              }
-
-              console.log('Unknown java file type', src);
-              reject('Unknown java file type'+src);
-            }, () => {
-              console.log('URL Not Found', src);
-              reject('Unknown java file type'+src);
-            });            
-          }          
-        })
-      );
-    });
-  }
-
-  dynamicMountJava(src) {
-    const Buffer = global.BrowserFS.BFSRequire('buffer').Buffer;
-    const options = this.getOptions();
     return new Promise((resolve, reject) => {
       if (CommonUtils.isZipFile(src)) {
         return this.mountFileViaXHR(src).then(
-          (jarStorePath) => WrapperUtil.dispatchOnJVM(this.javapoly, 'JAR_PATH_ADD', 10, ['file://'+jarStorePath], resolve, reject) ,
+          fRegJarPath.bind(this, resolve, reject),
           reject );
       } else {
         // remote file, we need to download the file data of that url and parse the type.
-        CommonUtils.xhrRetrieve(src, "arraybuffer").then(fileData => {
+        CommonUtils.xhrRetrieve(src, 'arraybuffer').then(fileData => {
           const fileDataBuf = new Buffer(fileData);
 
           // remote java class/jar file
           if (CommonUtils.isClassFile(fileDataBuf)){
             //return WrapperUtil.dispatchOnJVM(this, 'FS_MOUNT_CLASS', 10, {src:script.src});
             return this.writeRemoteClassFileIntoFS(src, fileDataBuf).then(resolve, reject);
-          } else if (CommonUtils.isZipFile(fileDataBuf)){
+          } else if (CommonUtils.isZipFile(fileDataBuf)) {
             //return WrapperUtil.dispatchOnJVM(this, 'FS_MOUNT_JAR', 10, {src:script.src});
             return this.writeRemoteJarFileIntoFS(src, fileDataBuf).then(
-              (jarStorePath) => WrapperUtil.dispatchOnJVM(this.javapoly, 'JAR_PATH_ADD', 10, ['file://'+jarStorePath], resolve, reject) ,
-              reject);
+              fRegJarPath.bind(this, resolve, reject),
+              reject );
           }
 
           // remote java source code file
-          const classInfo = CommonUtils.detectClassAndPackageNames(fileDataBuf.toString()) ;
-          if (classInfo && classInfo.class ){
+          const classInfo = CommonUtils.detectClassAndPackageNames(fileDataBuf.toString());
+          if (classInfo && classInfo.class) {
             const className = classInfo.class;
             const packageName = classInfo.package;
             return WrapperUtil.dispatchOnJVM(
-                this.javapoly, "FILE_COMPILE", 10,
-                [className, packageName ? packageName : "", options.storageDir, fileDataBuf.toString()], resolve, reject
-              );
+              this.javapoly, 'FILE_COMPILE', 10,
+              [className, packageName ? packageName : '', options.storageDir, fileDataBuf.toString()], resolve, reject
+            );
           }
 
           console.log('Unknown java file type', src);
@@ -165,26 +117,44 @@ class DoppioManager {
         }, () => {
           console.log('URL Not Found', src);
           reject('Unknown java file type'+src);
-        });
-      }
-    })
+        });            
+      }          
+    });
+  }
+
+  mountJava(src) {
+    this.bfsReady.then(() => {
+      this.mountHub.push(
+        this._mountJava(src, 
+          (resolve, reject, jarStorePath) => {this.classpath.push(jarStorePath); resolve();}
+        )
+      )
+    });
+  }
+
+  dynamicMountJava(src) {
+    const Buffer = global.BrowserFS.BFSRequire('buffer').Buffer;
+    const options = this.getOptions();
+    return this._mountJava(src, 
+      (resolve, reject, jarStorePath) => WrapperUtil.dispatchOnJVM(this.javapoly, 'JAR_PATH_ADD', 10, ['file://' + jarStorePath], resolve, reject)
+    ); 
   }
   
   mountFileViaXHR(src) {
     const options = this.getOptions();
     
     return new Promise((resolve, reject) => {
-      const fileName = this.path.basename(src);
-
-      let listingObject = {}; listingObject[fileName] = null;
-      
-      let lastSlash = 0; 
-      for(let ti = 0; (ti = src.indexOf('/', lastSlash + 1)) > 0; lastSlash = ti) ;
-      
-      const mountPoint = new BrowserFS.FileSystem.XmlHttpRequest(listingObject, src.substr(0,lastSlash));
-      
+      const fileName = this.path.basename(src);    
       const dirName = this.path.join(src.replace(/[\///\:]/gi, ''));
-      this.xhrdirs.mount('/' + dirName, mountPoint);
+
+      if (!this.fs.existsSync('/xhrdirs/' + dirName)) {
+        let listingObject = {}; listingObject[fileName] = null;
+        let lastSlash = 0; 
+        for(let ti = 0; (ti = src.indexOf('/', lastSlash + 1)) > 0; lastSlash = ti) ;            
+        const mountPoint = new BrowserFS.FileSystem.XmlHttpRequest(listingObject, src.substr(0,lastSlash));        
+        
+        this.xhrdirs.mount('/' + dirName, mountPoint);        
+      }      
       
       resolve(this.path.join('/xhrdirs', dirName, fileName));
     });
