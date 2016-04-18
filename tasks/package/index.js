52d28fefb1c98158ae18929d798848396457778b
Russkih Anatoly <titanofdarkness@gmail.com>
2016-04-18 20:35:16 +1000

    Refactoring loadmodule function

--- tasks/package/index.js
+++ tasks/package/index.js
@@ -3,31 +3,21 @@ if (typeof window === "undefined") {
     console.log("JavaPolyNodeSystem running in Node");
 
     var loadModule = function() {
-        var paths = ['./javapoly-node-system-raw.js', './package/javapoly-node-system-raw.js', 'JavaPoly/javapoly-node-system-raw.js'];
+        var path = require('path');
+        var currentDir = __dirname;
+        var javaPolyPath = path.join(currentDir, 'javapoly-node-system-raw.js');
         var result;
-        var fs = require('fs');
-        var errors = [];
-        paths.forEach(function (path) {
-            try {
-                result = require(path);
-            } catch (e) {
-                errors.push(e);
-            }
-        });
-        if (result) {
-            return result;
-        } else {
-            errors.forEach(function (e) {
-                console.error("Error requesting module on path: %s", path);
-                console.error(e);
-            })
+        try {
+            result = require(javaPolyPath);
+        } catch (e) {
+            console.error('Cannot load module on path: %s', javaPolyPath);
         }
+        return result;
     };
 
     // Constructor
     const JavaPolyNodeSystem = loadModule();
     if (JavaPolyNodeSystem) {
-
         // For running this code as Node module
         module.exports = JavaPolyNodeSystem;
     } else {
