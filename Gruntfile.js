db3e79de1efb874474362777b47beb1734c5668d
hrjet9 <hrjet9@gmail.com>
2016-05-10 14:35:42 +0530

    Temporarily disable the newer plugin, since it is not working for systemjvm compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,7 +245,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build:test', ['mkdir:build', 'build:java', 'run_java:compile-test', 'compare_version', 'browserify:development', 'listings:javapoly', 'symlink:build_to_test']);
     grunt.registerTask('build', ['mkdir:build', 'build:java', 'newer:browserify:production', 'newer:browserify:node-doppio', 'listings:javapoly']);
     grunt.registerTask('build:node-doppio', ['mkdir:build', 'build:java', 'run_java:compile-test', 'newer:browserify:node-doppio']);
-    grunt.registerTask('build:node-system', ['mkdir:build', 'build:java', 'run_java:compile-test', 'newer:browserify:node-system']);
+    grunt.registerTask('build:node-system', ['mkdir:build', 'build:java', 'run_java:compile-test', 'browserify:node-system']);
     grunt.registerTask('build:browser', ['mkdir:build', 'build:java', 'newer:browserify:production', 'listings:javapoly']);
 
     grunt.registerTask('default', ['build']);
